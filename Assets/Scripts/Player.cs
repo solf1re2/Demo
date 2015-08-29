f82d3e73a2e9f15d7ddbb593cae572f0e7fffcc5
James <jy1v07@gmail.com>
2015-08-29 08:46:49 +0100

    extra sprites and character movement
    added extra sprites for terrain tiles and added simple movement for
    player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,21 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            this.transform.Translate(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector2.left);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector2.right);
+        }
+
+    }
 }
